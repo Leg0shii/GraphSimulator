683156ba2a8078e7fb7b4db680d5293c741be345
0hnezahn <supidupi12@yahoo.de>
2023-09-12 09:18:12 +0200

    Initial commit

---
+++ src/main/java/de/legoshi/graphsimulator/Controller.java
@@ -0,0 +1,16 @@
+package de.legoshi.graphsimulator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+}
---
+++ src/main/java/de/legoshi/graphsimulator/Main.java
@@ -0,0 +1,9 @@
+package de.legoshi.graphsimulator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+
+}
