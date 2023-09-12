6bffe8209efb445c77297b511656ea133c1ea941
0hnezahn <supidupi12@yahoo.de>
2023-09-12 09:29:05 +0200

    add standard font to fix missing mac font

--- src/main/java/de/legoshi/graphsimulator/Application.java
+++ src/main/java/de/legoshi/graphsimulator/Application.java
@@ -7,10 +7,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(Application.class.getResource(("stylesheet.css")).toExternalForm());
         stage.setTitle("GraphSimulator");
         stage.setScene(scene);
         stage.show();
