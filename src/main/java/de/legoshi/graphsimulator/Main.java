3de309a47951e9917cf18d85bb55b3cd276bf662
0hnezahn <benjamin.mueller2@uni-rostock.de>
2023-09-15 23:04:56 +0200

    shrink screen, add maven shading

--- src/main/java/de/legoshi/graphsimulator/Main.java
+++ src/main/java/de/legoshi/graphsimulator/Main.java
@@ -1,19 +1,9 @@
 package de.legoshi.graphsimulator;
 
-import de.legoshi.graphsimulator.plot.Distribution;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
         Application.main(args);
-        
-        List<Distribution> list = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            list.add(new Distribution(i, 1));
-        }
     }
 
 }
